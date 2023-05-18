51c3fa6d8121b8641c21eb98b8b1e9136007d96b
elyon.ortiz <elyon.ortiz08@gamil.com>
2023-05-18 14:03:48 -0400

    backup 01:34:36

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/momories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
+app.register(memoriesRoutes)
 
-  return users
-})
 app
   .listen({
     port: 3333,
