c29f9b805c5047b30de8d961c57fb0d92b900983
Elyon <elyon.ortiz08@gmail.com>
2023-07-17 18:57:43 -0400

    lesson 4 completed

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import 'dotenv/config'
 
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/momories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(require('@fastify/static'),{
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
+
 app.register(cors, {
   origin: true,
 })
@@ -15,6 +25,7 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
